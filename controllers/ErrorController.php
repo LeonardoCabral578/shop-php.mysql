5b0deaa9b8397b540ec733619a39a06ef4741039
Leo <leo.cabral1016@gmail.com>
2021-02-09 20:04:58 -0300

    rutas amigables

---
+++ controllers/ErrorController.php
@@ -0,0 +1,11 @@
+<?php
+
+class ErrorController {
+
+    # FUN
+    #---------------------------------------------------#
+        public function index() {
+            echo "<h1>La página que buscas no existe</h1>";
+        }
+
+}
