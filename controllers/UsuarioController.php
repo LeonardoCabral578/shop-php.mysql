d7b08db759442f80e34301d1ac5130e7222bf3d1
Leo <leo.cabral1016@gmail.com>
2021-02-09 23:43:33 -0300

    arreglos1

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -27,34 +27,23 @@ class UsuarioController {
                 #---------------------------------------------------#
 
                 // NOMBRE
-                if (!empty($nombre) && !is_numeric($nombre) && !preg_match("/[0-9]/", $nombre)) {
-                    $nombre_validado = true;
-                } else {
-                    $nombre_validado = false;
+                if (empty($nombre) || is_numeric($nombre) || preg_match("/[0-9]/", $nombre)) {
                     $errores['nombre'] = "El nombre no es válido";
-                }
+                } 
+
 
                 // APELLIDOS
-                if (!empty($apellidos) && !is_numeric($apellidos) && !preg_match("/[0-9]/", $apellidos)) {
-                    $apellidos_validado = true;
-                } else {
-                    $apellidos_validado = false;
+                if (empty($apellidos) || is_numeric($apellidos) || preg_match("/[0-9]/", $apellidos)) {
                     $errores['apellidos'] = "El apellido no es válido";
                 }
 
                 // EMAIL
-                if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $email_validado = true;
-                } else {
-                    $email_validado = false;
+                if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     $errores['email'] = "El email no es válido";
                 }
 
                 // PASSWORD
-                if (!empty($password)) {
-                    $password_validado = true;
-                } else {
-                    $password_validado = false;
+                if (empty($password)) {
                     $errores['password'] = "La contraseña está vacía";
                 }
 
--- helpers/fun.php
+++ helpers/fun.php
@@ -2,34 +2,23 @@
 
 # ERRORES
 #-------------------------------------------------------------------------------------------#
-function mostrarError($errores, $campo){
+    function mostrarError($errores, $campo){
 
-    $alerta = '';
-    if (isset($errores[$campo]) && !empty($campo)) {
-        $alerta = "<div class='alert_red'>" . $errores[$campo] . '</div>';
+        $alerta = '';
+        if (isset($errores[$campo]) && !empty($campo)) {
+            $alerta = "<div class='alert_red'>" . $errores[$campo] . '</div>';
+        }
+
+        return $alerta;
     }
 
-    return $alerta;
-}
+    function borrarErrores(){
+        $borrado = false;
 
-function borrarErrores(){
-    $borrado = false;
+        if (isset($_SESSION['errores'])) {
+            $_SESSION['errores'] = null;
+            $borrado = true;
+        }
 
-    if (isset($_SESSION['errores'])) {
-        $_SESSION['errores'] = null;
-        $borrado = true;
+        return $borrado;
     }
-
-    if (isset($_SESSION['errores_entrada'])) {
-        $_SESSION['errores_entrada'] = null;
-        $borrado = true;
-    }
-
-    // Ojo aca, en el video le colocó un parámetro, pero no me funciona.
-    if (isset($_SESSION['completado'])) {
-        $_SESSION['completado'] = null;
-        $borrado = true;
-    }
-
-    return $borrado;
-}
